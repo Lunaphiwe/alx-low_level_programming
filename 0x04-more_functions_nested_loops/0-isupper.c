2e331208be509c3d4cbc5bbeaf2ab780be0bb4ab
Aphiwe <matiniseaphiwe@gmail.com>
2023-11-10 23:36:33 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase alphabets
+ * @c: input character
+ * Return: (1) if uppercase and (0) otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else if
+	{
+		return (0);
+	}
+}
