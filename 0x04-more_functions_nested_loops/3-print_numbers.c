df253ae33746f055088343f0215d332e2ab70362
Aphiwe <matiniseaphiwe@gmail.com>
2023-11-13 00:10:01 -0800

    printing_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 - 9
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
